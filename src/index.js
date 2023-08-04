79ff72608f60f0ff1b18f23fa754a0c13bf6a5a0
dargonj24 <dargonj24@gmail.com>
2023-08-04 18:43:23 +0700

    how use routes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 const express =require('express')
 const app=express()
 const port=3000
+/* method routes : 
+get :  request data
+post : create or update
+put : update  require full filed
+path : update only  filed you want update
+delete : delete data
+*/
 app.get("/",(req,res)=>{
   res.send("hello world")
 })
