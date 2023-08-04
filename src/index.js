aaa6db575c2648fd733bb5bc078ea53c8f9d52cf
dargonj24 <dargonj24@gmail.com>
2023-08-04 20:05:11 +0700

    static file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import express from "express"
 import { engine} from 'express-handlebars'
 const app=express()
+//setup static file
+app.use(express.static('src/public'))
 //setup express-handlebar
 app.engine('.hbs',engine({extname:'.hbs'}))
 app.set("view engine",".hbs")
